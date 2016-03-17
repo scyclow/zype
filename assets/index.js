e9591d84ece2545230be88dbaa955fbe77c84b6b
scyclow <spike716@gmail.com>
2016-03-17 12:39:34 -0400

    initial commit

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Bleh = function Bleh() {
+  _classCallCheck(this, Bleh);
+
+  return 'bleh';
+};
+//# sourceMappingURL=index.js.map
---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+class Bleh {
+  constructor() {
+    return 'bleh'
+  }
+}
