1aa1e05733f677bb33a793915da8a92f8100a233
scyclow <spike716@gmail.com>
2016-03-17 13:20:34 -0400

    populate data

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,27 @@
 'use strict';
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var API_KEY = 'H7CF2IHbEc6QIrMVwb2zfd9VI14HHGAfYax1eHEUsJ4voYuqWF2oWvByUOhERva_';
+var $thumbnailContainer = document.getElementById('thumbnail-container');
 
-var Bleh = function Bleh() {
-  _classCallCheck(this, Bleh);
-
-  return 'bleh';
+var prepData = function prepData(payload) {
+  return payload.data.response;
 };
+
+function renderThumbnails(videos) {
+  var thumbnails = videos.map(function (video) {
+    return '\n    <div class="thumbnail">\n      <img src="' + video.thumbnails[0].url + '" />\n      <span>' + video.title + '</span>\n    </div>\n  ';
+  });
+
+  return thumbnails.join('');
+}
+
+function mountThumbnails(content) {
+  $thumbnailContainer.innerHTML = content;
+}
+
+function populateThumbnails() {
+  axios.get('https://api.zype.com/videos/?api_key=' + API_KEY).then(prepData).then(renderThumbnails).then(mountThumbnails);
+}
+
+window.onload = populateThumbnails;
 //# sourceMappingURL=index.js.map
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,29 @@
-class Bleh {
-  constructor() {
-    return 'bleh'
-  }
+const API_KEY = 'H7CF2IHbEc6QIrMVwb2zfd9VI14HHGAfYax1eHEUsJ4voYuqWF2oWvByUOhERva_';
+const $thumbnailContainer = document.getElementById('thumbnail-container');
+
+const prepData = (payload) => payload.data.response;
+
+function renderThumbnails(videos) {
+  const thumbnails = videos.map(video => `
+    <div class="thumbnail">
+      <img src="${video.thumbnails[0].url}" />
+      <span>${video.title}</span>
+    </div>
+  `);
+
+  return thumbnails.join('');
 }
+
+function mountThumbnails(content) {
+  $thumbnailContainer.innerHTML = content;
+}
+
+function populateThumbnails() {
+  axios
+    .get(`https://api.zype.com/videos/?api_key=${API_KEY}`)
+    .then(prepData)
+    .then(renderThumbnails)
+    .then(mountThumbnails);
+}
+
+window.onload = populateThumbnails;
