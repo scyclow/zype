ab79cae9fe16ecbd5c56ccfb1fe9a57fa38dccdb
scyclow <spike716@gmail.com>
2016-03-18 17:58:52 -0400

    MVCify

--- assets/index.js
+++ assets/index.js
@@ -1,33 +1,83 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 var API_KEY = 'H7CF2IHbEc6QIrMVwb2zfd9VI14HHGAfYax1eHEUsJ4voYuqWF2oWvByUOhERva_';
-var $thumbnailContainer = document.getElementById('thumbnail-container');
 var imgQuality = 1;
 
-var prepData = function prepData(payload) {
-  return payload.data.response;
-};
+// Model
 
-// Could be cleaned up with lodash's `get` function
-var getImgUrl = function getImgUrl(video) {
-  return video.thumbnails && video.thumbnails.length && video.thumbnails[imgQuality] && video.thumbnails[imgQuality].url || 'default-thumbnail.png';
-};
+var Video = function () {
+  _createClass(Video, null, [{
+    key: 'requestVideos',
+    value: function requestVideos() {
+      var formatVideos = function formatVideos(payload) {
+        var data = payload.data.response;
+        return Video.all = data.map(function (videoObj) {
+          return new Video(videoObj, imgQuality);
+        });
+      };
 
-function renderThumbnails(videos) {
-  var thumbnails = videos.map(function (video) {
-    return '\n    <div class="thumbnail">\n      <img class="thumbnail-img" src="' + getImgUrl(video) + '" />\n      <span class="caption">' + video.title + '</span>\n    </div>\n  ';
+      return axios.get('https://api.zype.com/videos/?api_key=' + API_KEY).then(formatVideos);
+    }
+  }]);
+
+  function Video(data, imgQuality) {
+    _classCallCheck(this, Video);
+
+    this.title = data.title;
+    // Could be cleaned up with lodash's `get` function
+    this.imgUrl = data.thumbnails && data.thumbnails.length && data.thumbnails[imgQuality] && data.thumbnails[imgQuality].url || 'default-thumbnail.png';
+  }
+
+  return Video;
+}();
+
+// View
+
+
+var Thumnails = function () {
+  function Thumnails(videos, containerId) {
+    _classCallCheck(this, Thumnails);
+
+    this.videos = videos;
+    this.thumbnailContainer = document.getElementById(containerId);
+  }
+
+  _createClass(Thumnails, [{
+    key: 'render',
+    value: function render() {
+      var thumbnails = this.videos.map(function (video) {
+        return '\n      <div class="thumbnail">\n        <img class="thumbnail-img" src="' + video.imgUrl + '" />\n        <span class="caption">' + video.title + '</span>\n      </div>\n    ';
+      });
+
+      this.thumbnailContainer.innerHTML = thumbnails.join('');
+    }
+  }]);
+
+  return Thumnails;
+}();
+
+// Controller
+
+
+var ThumbnailPage = function ThumbnailPage(mountId) {
+  var _this = this;
+
+  _classCallCheck(this, ThumbnailPage);
+
+  this.mountId = mountId;
+
+  return Video.requestVideos().then(function (videos) {
+    _this.videos = videos;
+    _this.view = new Thumnails(_this.videos, _this.mountId);
+    _this.view.render();
   });
+};
 
-  return thumbnails.join('');
-}
-
-function mountThumbnails(content) {
-  $thumbnailContainer.innerHTML = content;
-}
-
-function populateThumbnails() {
-  axios.get('https://api.zype.com/videos/?api_key=' + API_KEY).then(prepData).then(renderThumbnails).then(mountThumbnails);
-}
-
-window.onload = populateThumbnails;
+window.onload = function () {
+  return new ThumbnailPage('thumbnail-container');
+};
 //# sourceMappingURL=index.js.map
--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,62 @@
 const API_KEY = 'H7CF2IHbEc6QIrMVwb2zfd9VI14HHGAfYax1eHEUsJ4voYuqWF2oWvByUOhERva_';
-const $thumbnailContainer = document.getElementById('thumbnail-container');
 const imgQuality = 1;
 
-const prepData = (payload) => payload.data.response;
+// Model
+class Video {
+  static requestVideos() {
+    const formatVideos = (payload) => {
+      const data = payload.data.response;
+      return Video.all = data.map(videoObj => new Video(videoObj, imgQuality));
+    }
 
-// Could be cleaned up with lodash's `get` function
-const getImgUrl = (video) => (
-    video.thumbnails &&
-    video.thumbnails.length &&
-    video.thumbnails[imgQuality] &&
-    video.thumbnails[imgQuality].url
-  ) || 'default-thumbnail.png';
+    return axios
+      .get(`https://api.zype.com/videos/?api_key=${API_KEY}`)
+      .then(formatVideos);
+  }
 
-function renderThumbnails(videos) {
-  const thumbnails = videos.map(video => `
-    <div class="thumbnail">
-      <img class="thumbnail-img" src="${getImgUrl(video)}" />
-      <span class="caption">${video.title}</span>
-    </div>
-  `);
-
-  return thumbnails.join('');
+  constructor(data, imgQuality) {
+    this.title = data.title;
+    // Could be cleaned up with lodash's `get` function
+    this.imgUrl = (
+      data.thumbnails &&
+      data.thumbnails.length &&
+      data.thumbnails[imgQuality] &&
+      data.thumbnails[imgQuality].url
+    ) || 'default-thumbnail.png';
+  }
 }
 
-function mountThumbnails(content) {
-  $thumbnailContainer.innerHTML = content;
+// View
+class Thumnails {
+  constructor(videos, containerId) {
+    this.videos = videos;
+    this.thumbnailContainer = document.getElementById(containerId);
+  }
+
+  render() {
+    const thumbnails = this.videos.map(video => `
+      <div class="thumbnail">
+        <img class="thumbnail-img" src="${video.imgUrl}" />
+        <span class="caption">${video.title}</span>
+      </div>
+    `);
+
+    this.thumbnailContainer.innerHTML = thumbnails.join('');
+  }
 }
 
-function populateThumbnails() {
-  axios
-    .get(`https://api.zype.com/videos/?api_key=${API_KEY}`)
-    .then(prepData)
-    .then(renderThumbnails)
-    .then(mountThumbnails);
+// Controller
+class ThumbnailPage {
+  constructor(mountId) {
+    this.mountId = mountId;
+
+    return Video.requestVideos()
+      .then(videos => {
+        this.videos = videos;
+        this.view = new Thumnails(this.videos, this.mountId);
+        this.view.render();
+      });
+  }
 }
 
-window.onload = populateThumbnails;
+window.onload = () => new ThumbnailPage('thumbnail-container');
